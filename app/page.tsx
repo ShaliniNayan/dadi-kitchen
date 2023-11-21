c21af72bac8fefce37dc5d335f4d2b9e4fed333f
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-11-21 21:21:16 +0530

    Update nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
-import Link from "next/link"
+import Link from 'next/link';
 
 export default function Home() {
   return (
-   <>
-   <header>
-    <a href="">Dadi's Kitchen</a>
-    <nav>
-      <Link href={''}>Home</Link>
-      <Link href={''}>Menu</Link>
-      <Link href={''}>About</Link>
-      <Link href={''}>Contact</Link>
-      <Link href={''}>Login</Link>
-    </nav>
-   </header>
-   </>
-  )
+    <>
+      <header className='flex items-center'>
+        <Link className='text-black-400 font-semibold text-2xl' href=''>
+          DADI's KITCHEN
+        </Link>
+
+        <nav className='flex'>
+          <Link href={''}>Home</Link>
+          <Link href={''}>Menu</Link>
+          <Link href={''}>About</Link>
+          <Link href={''}>Contact</Link>
+          <Link href={''}>Login</Link>
+        </nav>
+      </header>
+    </>
+  );
 }
