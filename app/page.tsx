3b2dce9b8ed9c90b5ceb5de301cfd3aff82ab88d
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-11-21 20:53:18 +0530

    Add nav and Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <div>test</div>
+   <>
+   <header>
+    <a href="">Dadi's Kitchen</a>
+    <nav>
+      <Link href={''}>Home</Link>
+      <Link href={''}>Menu</Link>
+      <Link href={''}>About</Link>
+      <Link href={''}>Contact</Link>
+      <Link href={''}>Login</Link>
+    </nav>
+   </header>
+   </>
   )
 }
